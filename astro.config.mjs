0166a61bd758e5a5d663d9b3a375bc0fe4eb5734
Reece Atkinson <hello@reeceatkinson.com>
2022-12-04 15:10:53 +0000

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,17 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://reeceatkinson.com/",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), sitemap()],
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
-  },
+      external: ["svgo"]
+    }
+  }
 });
